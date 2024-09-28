16900a037c95702e429e95de4c145b28cb5f49de
rui623 <ruiiwata837@gmail.com>
2024-09-28 14:55:14 +0900

    add publish function

--- rust/rust_subscriber.py
+++ rust/rust_subscriber.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image  # 画像データを受信
-from std_msgs.msg import String  # 結果を返す
+from std_msgs.msg import Float64 #結果を返す
 from cv_bridge import CvBridge,CvBridgeError
 import cv2
 import numpy as np
@@ -14,6 +14,8 @@ class ImageSubscriber(Node):
     def __init__(self):
         super().__init__('image_subscriber')
         self.subscription = self.create_subscription(Image, 'image_topic', self.image_callback, 0)
+        self.image_publisher = self.create_publisher(Image, 'rust_result_image', 10)
+        self.value_publisher = self.create_publisher(Float64, 'rust_result_value', 10)
         # self.result_publisher = self.create_publisher(String, 'result_topic', 10)
         self.bridge = CvBridge()
 
@@ -48,13 +50,18 @@ class ImageSubscriber(Node):
 
             # 線の太さ
             thickness = 2
-            cv2.putText(warped, text, (50,50), font, font_scale, color, thickness, cv2.LINE_AA)
+            cv2.putText(warped, text, (50,100), font, font_scale, color, thickness, cv2.LINE_AA)
             cv2.imshow("result",warped)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
             # 処理後、ノードをシャットダウンするか、トピックからの購読を解除
             # self.get_logger().info('Image processed successfully, shutting down node.')
-            
+            if area != None:
+                result_value = Float64()
+                result_value.data = area
+                self.value_publisher.publish(result_value)
+                ros_image = self.bridge.cv2_to_imgmsg(warped, 'bgr8')
+                self.image_publisher.publish(ros_image)
 
             
 
